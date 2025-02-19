04c85d23d7608220313708e5eed31d1b36bcf594
Yume Rokku <163641570+tuanloc782004@users.noreply.github.com>
2025-02-19 10:00:18 +0700

    first commit

---
+++ pbl5/src/test/java/com/example/pbl5/PasswordTests.java
@@ -0,0 +1,12 @@
+package com.example.pbl5;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordTests {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
+	}
+	
+}
---
+++ pbl5/src/test/java/com/example/pbl5/Pbl5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pbl5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pbl5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
