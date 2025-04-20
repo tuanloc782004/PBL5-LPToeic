dd185491c5b35b26855d98a7b1b621e94a62cd8c
TranThiThanhPhuong <phuongsuga333@gmail.com>
2025-04-20 23:56:15 +0700

    mockexam and mockexamcard

--- pbl5/src/main/java/com/pbl5/controller/user/MockExamController.java
+++ pbl5/src/main/java/com/pbl5/controller/user/MockExamController.java
@@ -1,5 +1,15 @@
 package com.pbl5.controller.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/mock-exam")
 public class MockExamController {
+	
+	@RequestMapping("")
+	public String cardTests () {
+		return "user/mock-exam"; 
+	}
 
 }
